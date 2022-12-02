b65a248dd912be8803d02304b3e85fc93bf6768a
Benjamin Maynor <BenjaminMaynor@gmail.com>
2022-12-02 09:39:02 -0600

    Day 2

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/btm6084/advent-of-code-2022/pkg/day1"
+	"github.com/btm6084/advent-of-code-2022/pkg/day2"
 )
 
 type Runner func() string
@@ -12,11 +13,15 @@ var (
 	runners = []Runner{
 		day1.Part1,
 		day1.Part2,
+		day2.Part1,
+		day2.Part2,
 	}
 
 	labels = []string{
 		"Day 1 Part 1",
 		"Day 1 Part 2",
+		"Day 2 Part 1",
+		"Day 2 Part 2",
 	}
 )
 
---
+++ pkg/day2/day2.go
@@ -0,0 +1,66 @@
+package day2
+
+import (
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/btm6084/advent-of-code-2022/pkg/config"
+	"github.com/spf13/cast"
+)
+
+type Empty struct{}
+
+var pkg string
+
+var (
+	score = map[string]int{
+		"A X": 4,
+		"A Y": 8,
+		"A Z": 3,
+		"B X": 1,
+		"B Y": 5,
+		"B Z": 9,
+		"C X": 7,
+		"C Y": 2,
+		"C Z": 6,
+	}
+
+	strategy = map[string]int{
+		"A X": 3, // L
+		"A Y": 4, // D
+		"A Z": 8, // W
+		"B X": 1, // L
+		"B Y": 5, // D
+		"B Z": 9, // W
+		"C X": 2, // L
+		"C Y": 6, // D
+		"C Z": 7, // W
+	}
+)
+
+func init() {
+	pkgRaw := strings.Split(reflect.TypeOf(Empty{}).PkgPath(), string(os.PathSeparator))
+
+	pkg = pkgRaw[len(pkgRaw)-1]
+}
+
+func Part1() string {
+	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
+
+	sum := 0
+	for _, line := range lines {
+		sum += score[line]
+	}
+	return cast.ToString(sum)
+}
+
+func Part2() string {
+	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
+
+	sum := 0
+	for _, line := range lines {
+		sum += strategy[line]
+	}
+	return cast.ToString(sum)
+}
