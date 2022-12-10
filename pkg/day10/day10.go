3bfa919d08d4bda953faeb071ec6a82459951c15
Benjamin Maynor <BenjaminMaynor@gmail.com>
2022-12-10 11:17:33 -0600

    Day 10

--- pkg/day10/day10.go
+++ pkg/day10/day10.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 
 	"github.com/btm6084/advent-of-code-2022/pkg/config"
+	"github.com/btm6084/utilities/slice"
+	"github.com/spf13/cast"
 )
 
 type Empty struct{}
@@ -22,14 +24,88 @@ func init() {
 
 func Part1() string {
 	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
-	_ = lines
 
-	return "@todo"
+	cycles := 0
+	x := 1
+	signal := 0
+
+	for _, line := range lines {
+		if line == "noop" {
+			cycles++
+			signal += sum(x, cycles)
+			continue
+		}
+
+		cycles++
+		signal += sum(x, cycles)
+		n := cast.ToInt(strings.Split(line, " ")[1])
+		cycles++
+		signal += sum(x, cycles)
+		x += n
+	}
+
+	return cast.ToString(signal)
 }
 
 func Part2() string {
 	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
-	_ = lines
 
-	return "@todo"
+	x := 1
+
+	crt := make([]string, 240)
+
+	for i := 0; i < 240; i++ {
+		line := lines[0]
+		lines = lines[1:]
+
+		px := i % 40
+
+		if px >= x-1 && px <= x+1 {
+			// Draw
+			crt[i] = "#"
+		} else {
+			crt[i] = " "
+		}
+
+		if line == "noop" {
+			continue
+		}
+
+		n := cast.ToInt(strings.Split(line, " ")[1])
+
+		i++ // 2nd cycle
+		px = i % 40
+
+		if px >= x-1 && px <= x+1 {
+			// Draw
+			crt[i] = "#"
+		} else {
+			crt[i] = " "
+		}
+
+		x += n
+	}
+
+	return printCRT(crt)
+}
+
+func printCRT(crt []string) string {
+	out := "\n"
+	for i := 0; i < len(crt); i++ {
+		if i > 0 && i%40 == 0 {
+			out += "\n"
+		}
+
+		out += crt[i]
+	}
+
+	return out
+}
+
+func sum(x, cycle int) int {
+	if slice.ContainsInt([]int{20, 60, 100, 140, 180, 220}, cycle) {
+		return x * cycle
+	}
+
+	return 0
 }
