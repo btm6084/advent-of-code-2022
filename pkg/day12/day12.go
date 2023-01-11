de30dacec2b42f5a746b0552e36e2ef92241c16d
Benjamin Maynor <BenjaminMaynor@gmail.com>
2023-01-11 14:33:39 -0600

    Day 13 - Unfinished, no time

--- pkg/day12/day12.go
+++ pkg/day12/day12.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	"reflect"
+	"sort"
 	"strings"
 
 	"github.com/btm6084/advent-of-code-2022/pkg/config"
@@ -44,15 +45,10 @@ type Node struct {
 func Part1() string {
 	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
 
-	startX, startY := 0, 0
-	goalX, goalY := 0, 0
-
 	graph := make([][]*Node, len(lines))
 	for i := 0; i < len(lines); i++ {
 		graph[i] = make([]*Node, len(lines[i]))
 	}
-
-	fmt.Println(len(graph), len(graph[0]))
 
 	var toVisit []*Node
 	var goal *Node
@@ -87,11 +83,76 @@ func Part1() string {
 		toVisit = visit(graph, toVisit, i)
 	}
 
-	fmt.Printf("Start: [%2d,%2d]\n", startX, startY)
-	fmt.Printf("End: [%2d,%2d]\n", goalX, goalY)
-	fmt.Println()
-
 	return cast.ToString(goal.Cost)
+}
+
+func Part2() string {
+	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
+
+	type pt struct {
+		Row int
+		Col int
+	}
+
+	var starts []pt
+	for row, line := range lines {
+		for col := 0; col < len(line); col++ {
+			if line[col] == 'a' || line[col] == 'S' {
+				starts = append(starts, pt{Row: row, Col: col})
+			}
+		}
+	}
+
+	var costs []int
+	for _, p := range starts {
+		costs = append(costs, run(lines, p.Row, p.Col))
+	}
+
+	sort.Ints(costs)
+
+	return cast.ToString(costs[0])
+}
+
+func run(lines []string, startX, startY int) int {
+	graph := make([][]*Node, len(lines))
+	for i := 0; i < len(lines); i++ {
+		graph[i] = make([]*Node, len(lines[i]))
+	}
+
+	var toVisit []*Node
+	var goal *Node
+
+	for row, line := range lines {
+		for col := 0; col < len(line); col++ {
+			graph[row][col] = &Node{
+				ID:      fmt.Sprintf("[%d,%d]", row, col),
+				Height:  alpha[line[col]],
+				Cost:    999,
+				Visited: false,
+				Row:     row,
+				Col:     col,
+			}
+
+			if row == startX && col == startY {
+				graph[row][col].IsStart = true
+				graph[row][col].Parent = &Node{Cost: -1}
+				toVisit = append(toVisit, graph[row][col])
+			}
+
+			if line[col] == 'E' {
+				graph[row][col].IsGoal = true
+				goal = graph[row][col]
+			}
+		}
+	}
+
+	i := 0
+	for len(toVisit) > 0 {
+		i++
+		toVisit = visit(graph, toVisit, i)
+	}
+
+	return goal.Cost
 }
 
 func visit(graph [][]*Node, toVisit []*Node, i int) []*Node {
@@ -102,14 +163,11 @@ func visit(graph [][]*Node, toVisit []*Node, i int) []*Node {
 		return toVisit
 	}
 
-	// fmt.Printf("Visit [%d, %d] %d %d\n", node.Row, node.Col, len(toVisit), len(graph))
-
 	node.Cost = node.Parent.Cost + 1
 	node.Visited = true
 
 	if node.Row-1 >= 0 {
 		nn := graph[node.Row-1][node.Col]
-		// fmt.Printf("Up %t %d %t\n", nn.Visited, node.Height-nn.Height, !nn.Visited && node.Height-nn.Height >= -1)
 		if !nn.Visited && node.Height-nn.Height >= -1 {
 			nn.Parent = node
 			toVisit = append(toVisit, nn)
@@ -118,7 +176,6 @@ func visit(graph [][]*Node, toVisit []*Node, i int) []*Node {
 
 	if node.Row+1 < len(graph) {
 		nn := graph[node.Row+1][node.Col]
-		// fmt.Printf("Down %t %d %t\n", nn.Visited, node.Height-nn.Height, !nn.Visited && node.Height-nn.Height >= -1)
 		if !nn.Visited && node.Height-nn.Height >= -1 {
 			nn.Parent = node
 			toVisit = append(toVisit, nn)
@@ -127,7 +184,6 @@ func visit(graph [][]*Node, toVisit []*Node, i int) []*Node {
 
 	if node.Col-1 >= 0 {
 		nn := graph[node.Row][node.Col-1]
-		// fmt.Printf("Left %t %d %t\n", nn.Visited, node.Height-nn.Height, !nn.Visited && node.Height-nn.Height >= -1)
 		if !nn.Visited && node.Height-nn.Height >= -1 {
 			nn.Parent = node
 			toVisit = append(toVisit, nn)
@@ -136,7 +192,6 @@ func visit(graph [][]*Node, toVisit []*Node, i int) []*Node {
 
 	if node.Col+1 < len(graph[0]) {
 		nn := graph[node.Row][node.Col+1]
-		// fmt.Printf("Right %t %d %t\n", nn.Visited, node.Height-nn.Height, !nn.Visited && node.Height-nn.Height >= -1)
 		if !nn.Visited && node.Height-nn.Height >= -1 {
 			nn.Parent = node
 			toVisit = append(toVisit, nn)
@@ -144,35 +199,4 @@ func visit(graph [][]*Node, toVisit []*Node, i int) []*Node {
 	}
 
 	return toVisit
-}
-
-func Part2() string {
-	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
-	_ = lines
-
-	return "@todo"
-}
-
-func printInts(m [][]int) {
-	for row := 0; row < len(m); row++ {
-		for col := 0; col < len(m[row]); col++ {
-			fmt.Printf(" %3d ", m[row][col])
-		}
-		fmt.Println()
-	}
-	fmt.Println()
-}
-
-func printBools(m [][]bool) {
-	for row := 0; row < len(m); row++ {
-		for col := 0; col < len(m[row]); col++ {
-			if m[row][col] {
-				fmt.Print("  t  ")
-			} else {
-				fmt.Print("  f  ")
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Println()
 }
--- pkg/day13/day13.go
+++ pkg/day13/day13.go
@@ -1,11 +1,14 @@
 package day13
 
 import (
+	"fmt"
 	"os"
 	"reflect"
 	"strings"
 
 	"github.com/btm6084/advent-of-code-2022/pkg/config"
+	"github.com/btm6084/gojson"
+	"github.com/davecgh/go-spew/spew"
 )
 
 type Empty struct{}
@@ -23,6 +26,25 @@ func init() {
 func Part1() string {
 	lines := config.RawInput("./pkg/" + pkg + "/" + config.INPUTFILE)
 	_ = lines
+
+	for len(lines) >= 2 {
+		l := lines[:2]
+		lines = lines[2:]
+
+		var a []int
+		gojson.Unmarshal([]byte(l[0]), &a)
+
+		var b []int
+		gojson.Unmarshal([]byte(l[0]), &b)
+
+		spew.Dump(a, b)
+		fmt.Println()
+		fmt.Println()
+
+		if len(lines) > 2 {
+			lines = lines[1:]
+		}
+	}
 
 	return "@todo"
 }
